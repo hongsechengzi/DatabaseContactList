6f1aa82f4ee9d90368531be84fd8683a9ec5b77f
hongsechengzi <lanou3g@lianbinjing.local>
2014-06-13 20:40:50 +0800

    Initial Commit

---
+++ 作业16数据库通讯录/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  作业16数据库通讯录
+//
+//  Created by lanou3g on 14-6-13.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 作业16数据库通讯录Tests/__16______Tests.m
@@ -0,0 +1,34 @@
+//
+//  __16______Tests.m
+//  作业16数据库通讯录Tests
+//
+//  Created by lanou3g on 14-6-13.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __16______Tests : XCTestCase
+
+@end
+
+@implementation __16______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
